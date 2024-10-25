be812af74b5913e9b5fa909ed364f61d5de0e39e
ERHGDEV <erhgdev@gmail.com>
2024-10-25 17:40:54 -0600

    se implementa userpanel, se conserva jsPDF para reporte

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -109,7 +109,7 @@ const AdminPanel = () => {
     return (
       <div className="min-h-screen">
         <Header />
-        <main className="container mx-auto px-4 py-8">
+        <main className="mx-auto px-4 py-8">
           <div className="flex justify-between items-center mb-6">
             <h1 className="text-3xl font-bold">Panel de Administrador</h1>
           </div>
