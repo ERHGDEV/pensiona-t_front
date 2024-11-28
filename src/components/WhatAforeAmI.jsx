97f669306a0c53420b5bc8bed91723cb37939a31
ERHGDEV <erhgdev@gmail.com>
2024-11-28 17:04:00 -0600

    Se retira array de info de afore y únicamente se importa

--- src/components/WhatAforeAmI.jsx
+++ src/components/WhatAforeAmI.jsx
@@ -1,20 +1,8 @@
 import { useState, useEffect } from 'react'
 import axiosInstance from '../services/axiosConfig'
-import Dots from "../components/Dots"
-import Button from "../components/Button"
-
-const AFORE_INFO = {
-  '530': { name: 'XXI BANORTE', logo: '/afore-logos/xxi-banorte.png' },
-  '534': { name: 'PROFUTURO', logo: '/afore-logos/profuturo.png' },
-  '538': { name: 'PRINCIPAL', logo: '/afore-logos/principal.png' },
-  '544': { name: 'SURA', logo: '/afore-logos/sura.png' },
-  '550': { name: 'INBURSA', logo: '/afore-logos/inbursa.png' },
-  '552': { name: 'CITIBANAMEX', logo: '/afore-logos/citibanamex.png' },
-  '556': { name: 'AZTECA', logo: '/afore-logos/azteca.png' },
-  '562': { name: 'INVERCAP', logo: '/afore-logos/invercap.png' },
-  '568': { name: 'COPPEL', logo: '/afore-logos/coppel.png' },
-  '578': { name: 'PENSIONISSSTE', logo: '/afore-logos/pensionissste.png' },
-}
+import Dots from "./Dots"
+import Button from "./Button"
+import { AFORE_INFO } from '../constants/infoAfore'
 
 const WhatAforeAmI = () => {
   const [nss, setNss] = useState('')
