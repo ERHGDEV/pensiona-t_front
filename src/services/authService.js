332cb4ff9cd72e0af5197a9afbbe76c6414234cd
ERHGDEV <erhgdev@gmail.com>
2025-01-28 12:08:26 -0600

    Se agrega manejo de refresh token

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,14 +4,16 @@ import URL from '../constants/url'
 class AuthService {
   async login(email, password) {
     const response = await axios.post(`${URL}/login`, { email, password })
-    if (response.data.token) {
+    if (response.data.token && response.data.refreshToken) {
       this.setToken(response.data.token)
+      this.setRefreshToken(response.data.refreshToken)
     }
     return response.data
   }
 
   logout() {
     localStorage.removeItem('token')
+    localStorage.removeItem('refreshToken')
   }
 
   setToken(token) {
@@ -21,6 +23,32 @@ class AuthService {
   getToken() {
     return localStorage.getItem('token')
   }
+
+  setRefreshToken(refreshToken) {
+    localStorage.setItem('refreshToken', refreshToken)
+  }
+
+  getRefreshToken() {
+    return localStorage.getItem('refreshToken')
+  }
+
+  async refreshToken() {
+    const refreshToken = this.getRefreshToken();
+    if (!refreshToken) throw new Error('No refresh token available');
+    
+    try {
+        const response = await axios.post(`${URL}/refresh-token`, { refreshToken });
+        if (response.data.accessToken) {
+            this.setToken(response.data.accessToken);
+            return response.data.accessToken;
+        }
+        throw new Error('No access token returned');
+    } catch (error) {
+        console.error('Error al renovar el token:', error);
+        throw error;
+    }
+  }
+
 
   isAuthenticated() {
     return !!this.getToken()
