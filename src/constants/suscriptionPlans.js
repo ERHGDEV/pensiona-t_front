0b3c89faf98570b9fc0e9708bc342529129b9637
ERHGDEV <erhgdev@gmail.com>
2025-01-31 15:14:49 -0600

    Planes de suscripción

---
+++ src/constants/suscriptionPlans.js
@@ -0,0 +1,34 @@
+const PLANS = [
+    {
+      title: 'Free',
+      price: 0,
+      features: [
+        '📊 Proyección de pensión: 1/día',
+        '📄 Reporte detallado: 1/día',
+        '🔍 Consulta individual de Afore: 1/día',
+      ],
+    },
+    {
+      title: 'Pro',
+      price: 149,
+      features: [
+        '🧮 Análisis de semanas cotizadas: 5/día',
+        '📊 Proyección de pensión: 10/día',
+        '📄 Reporte detallado: 10/día',
+        '🔍 Consulta individual de Afore: 10/día',
+      ],
+    },
+    {
+      title: 'Unlimited',
+      price: 199,
+      features: [
+        '🧮 Análisis de semanas cotizadas: Ilimitado',
+        '📊 Proyección de pensión: Ilimitado',
+        '📄 Reporte detallado: Ilimitado',
+        '🔍 Consulta individual de Afore: Ilimitado',
+        '📂 Consulta masiva de Afore: Ilimitado',
+      ],
+    },
+]
+
+export default PLANS
