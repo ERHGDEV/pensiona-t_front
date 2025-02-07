d79eaf36829f21361c6345619fd4ea6e5359afee
ERHGDEV <erhgdev@gmail.com>
2025-02-07 13:17:12 -0600

    Se agrega sección de detalle pagos

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -11,11 +11,13 @@ import axiosInstance from "../services/axiosConfig"
 import CalculatorParameters from "../components/CalculatorParameters"
 import AdminEmailForm from "../components/AdminEmailForm"
 import UserGrowth from "../components/UserGrowth"
+import PaymentList from "../components/PaymentList"
 
 const AdminPanel = () => {
   const [loading, setLoading] = useState(true)
   const [activeSection, setActiveSection] = useState('home')
   const [users, setUsers] = useState([])
+  const [payments, setPayments] = useState([])
   const { showNotification } = useNotificationContext()
   
   const navigate = useNavigate()
@@ -23,7 +25,8 @@ const AdminPanel = () => {
   const fetchUsers = async () => {
     try { 
       const response = await axiosInstance.get('/admin')
-      setUsers(response.data)
+      setUsers(response.data.users)
+      setPayments(response.data.payments)
     } catch (error) {
       console.error('Error fetching users:', error)
       showNotification('Error al obtener la lista de usuarios', 'error')
@@ -90,10 +93,16 @@ const AdminPanel = () => {
                 Home
             </button>
             <button
-                className={`px-4 py-2 rounded-lg ${activeSection === 'statistics' ? 'bg-sky-950 text-white font-semibold' : 'bg-gray-100 text-gray-700' }`}
+                className={`px-4 py-2 mr-4 rounded-lg ${activeSection === 'statistics' ? 'bg-sky-950 text-white font-semibold' : 'bg-gray-100 text-gray-700' }`}
                 onClick={() => setActiveSection('statistics')}
             >
                 Estadísticas
+            </button>
+            <button
+                className={`px-4 py-2 rounded-lg ${activeSection === 'payments' ? 'bg-sky-950 text-white font-semibold' : 'bg-gray-100 text-gray-700' }`}
+                onClick={() => setActiveSection('payments')}
+            >
+                Ingresos
             </button>
           </div>
         </div>
@@ -108,12 +117,14 @@ const AdminPanel = () => {
               handleNotification={showNotification} 
             />
           </>  
-        ) : (
+        ) : activeSection === 'statistics' ? (
           <>
             <UserGrowth users={users} />
             <UserActivity />
             {/* <AdminEmailForm /> */}
           </>
+        ) : (
+          <PaymentList payments={payments}/>
         )
         }
 
