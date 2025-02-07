2ec760ce2e78f16c92a9a931eaee73aec210a568
ERHGDEV <erhgdev@gmail.com>
2025-02-06 20:21:20 -0600

    Se ordena pagos

--- src/components/PaymentHistory.jsx
+++ src/components/PaymentHistory.jsx
@@ -5,13 +5,14 @@ import { statusNormalize } from "../utils/statusPaymentNormalize"
 const PaymentHistory = () => {
     const { user } = useUserContext()
     const { paymentHistory } = user
+    const sortedPayments = [...paymentHistory].sort((a, b) => new Date(b.date) - new Date(a.date))
 
     return (
         <>
             <h2 className="mt-4 text-lg font-semibold text-sky-950">Historial de Pagos</h2>
             <div className="mt-2 space-y-2 overflow-y-auto">
-                {paymentHistory.length > 0 ? (
-                    paymentHistory.map((payment, index) => (
+                {sortedPayments.length > 0 ? (
+                    sortedPayments.map((payment, index) => (
                         <div key={index} className="border-b pb-2">
                             <p className="text-gray-700"><span className="font-semibold">Fecha:</span> {formatDate(payment.date)}</p>
                             <p className="text-gray-700"><span className="font-semibold">Descripción:</span> {payment.description}</p>
@@ -21,7 +22,7 @@ const PaymentHistory = () => {
                                     ? 'text-green-600' 
                                     : payment.status === 'rejected' 
                                         ? 'text-red-600' 
-                                        : 'text-yellow-600'
+                                        : 'text-yellow-700'
                             }`}>
                                 Estatus: {statusNormalize(payment.status)}
                             </p>
