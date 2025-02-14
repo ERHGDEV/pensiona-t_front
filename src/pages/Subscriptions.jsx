0d56f3253483981639ac3bfd96ccf0b8d73814a8
ERHGDEV <erhgdev@gmail.com>
2025-02-13 20:57:53 -0600

    scroll al iniciar

--- src/pages/Subscriptions.jsx
+++ src/pages/Subscriptions.jsx
@@ -1,14 +1,18 @@
+import { useEffect } from 'react'
 import Footer from '../components/Footer'
 import Pricing from '../components/Pricing'
 
 const Subscriptions = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
+
     return (
         <>
             <Pricing />
             <div className='xl:fixed xl:bottom-0 xl:w-full'>
                 <Footer />
             </div>
-                
         </>
     )
 }
