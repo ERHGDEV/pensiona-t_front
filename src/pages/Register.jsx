6d44413d9cdd5d5fdce2664857968807c63508f7
ERHGDEV <erhgdev@gmail.com>
2025-02-15 10:21:43 -0600

    Se elimina notification

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -73,7 +73,6 @@ const Register = () => {
 
     return (
         <>
-            <Notification />
             <main>
                 {showModal && (
                     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
