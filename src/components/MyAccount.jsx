691e79a32fd99449b0c07e2d78a625c2c498320f
ERHGDEV <erhgdev@gmail.com>
2025-03-29 13:39:13 -0600

    Se utiliza UserPills

--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -3,26 +3,17 @@ import UserProfile from './UserProfile'
 import ActiveSuscription from './ActiveSubscription'
 import ComponentTransition from './ComponentTransition'
 import { AnimatePresence } from 'framer-motion'
+import UserPill from './UserPill'
 
 const MyAccount = () => {
     const [activeSection, setActiveSection] = useState('profile')
 
     return (
         <div className='text-sky-950 bg-white rounded-lg shadow-xl p-6 mt-4 max-w-md w-full mx-auto'>
-            <div className="flex justify-center mb-6">
-                <button
-                        className={`px-4 py-2 mr-4 rounded-full ${activeSection === 'profile' ? 'bg-sky-950 text-white font-semibold' : 'bg-gray-100 text-gray-700 shadow-md' }`}
-                        onClick={() => setActiveSection('profile')}
-                >
-                    Perfil
-                </button>
-                <button
-                        className={`px-4 py-2 rounded-full ${activeSection === 'history' ? 'bg-sky-950 text-white font-semibold' : 'bg-gray-100 text-gray-700 shadow-md' }`}
-                        onClick={() => setActiveSection('history')}
-                >
-                    Historial
-                </button>
-            </div>
+            <nav className="flex justify-center space-x-4 mb-6">
+                <UserPill activeSection={activeSection} setActiveSection={setActiveSection} section="profile" text="Perfil" />
+                <UserPill activeSection={activeSection} setActiveSection={setActiveSection} section="history" text="Historial" />
+            </nav>
 
             <AnimatePresence mode="wait">
                 {activeSection === 'profile' && (
