8a8de8598229d21b5d33d4a29336f1ce309437d6
ERHGDEV <erhgdev@gmail.com>
2025-03-28 23:33:42 -0600

    Se modifican color de bg, text y hover

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,8 +7,8 @@ const Button = ({ children, order, onClick, type, to }) => {
         flex items-center justify-center sm:max-w-fit sm:mx-auto`
     const orderClasses =
         order === 'primary'
-        ? 'bg-white hover:bg-sky-600 active:bg-sky-700 text-lg text-sky-900 hover:text-white'
-        : 'bg-sky-600 hover:bg-white active:bg-sky-700 text-lg text-white hover:text-sky-900 active:text-white'
+        ? 'bg-gray-100 hover:bg-white active:bg-sky-700 text-lg text-sky-900 active:text-white'
+        : 'bg-sky-600 hover:bg-sky-800 active:bg-sky-700 text-lg text-white hover:text-gray-100 active:text-white'
 
     if (to) {
         return (
