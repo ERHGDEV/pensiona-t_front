4c3d8e8d275a1b987378c167b76ccf184e5cd3d0
ERHGDEV <erhgdev@gmail.com>
2025-03-29 00:05:40 -0600

    Se modifica etiqueta de pill a Mi cuenta

--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -9,13 +9,12 @@ const MyAccount = () => {
 
     return (
         <div className='text-sky-950 bg-white rounded-lg shadow-xl p-6 mt-4 max-w-md w-full mx-auto'>
-            {/* Botones de navegación */}
             <div className="flex justify-center mb-6">
                 <button
                         className={`px-4 py-2 mr-4 rounded-full ${activeSection === 'profile' ? 'bg-sky-950 text-white font-semibold' : 'bg-gray-100 text-gray-700 shadow-md' }`}
                         onClick={() => setActiveSection('profile')}
                 >
-                    Cuenta
+                    Perfil
                 </button>
                 <button
                         className={`px-4 py-2 rounded-full ${activeSection === 'history' ? 'bg-sky-950 text-white font-semibold' : 'bg-gray-100 text-gray-700 shadow-md' }`}
@@ -25,7 +24,6 @@ const MyAccount = () => {
                 </button>
             </div>
 
-            {/* Secciones con transiciones */}
             <AnimatePresence mode="wait">
                 {activeSection === 'profile' && (
                     <ComponentTransition key="profile">
