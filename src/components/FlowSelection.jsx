b8512676dc0839e9e2dbd4472e3f6dd369e4cdd4
ERHGDEV <erhgdev@gmail.com>
2025-03-28 23:34:33 -0600

    Se homologa espacio entre elementos

--- src/components/FlowSelection.jsx
+++ src/components/FlowSelection.jsx
@@ -6,7 +6,7 @@ const FlowSelection = ({ onSelection }) => {
             <h3 className="text-lg font-semibold">¿Cómo deseas ingresas tus datos?</h3>
             <h2 className="text-sm mt-4">Salario Promedio, Semanas Cotizadas, Edad</h2>
 
-            <div className="mt-8 flex flex-col gap-4 max-w-fit mx-auto">
+            <div className="mt-6 flex flex-col gap-4 max-w-fit mx-auto">
                 <Button variant="primary" onClick={() => onSelection('calculatorForm')}>
                     Ingresar manualmente
                 </Button>
