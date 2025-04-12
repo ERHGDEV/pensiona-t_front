956d90a38439b54492246552757864c6f314942b
ERHGDEV <erhgdev@gmail.com>
2025-04-12 01:06:32 -0600

    Se agregan iconos y se modifica animación y posición de notificación

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,11 +1,13 @@
 import { createPortal } from 'react-dom'
+import { motion, AnimatePresence } from 'framer-motion'
 import { useNotificationContext } from '../context/NotificationContext'
-import ComponentTransition from './ComponentTransition'
+import AlertRoundedIcon from './icons/AlertRoundedIcon'
+import CheckRoundedIcon from './icons/CheckRoundedIcon'
+import XSquareRoundedIcon from './icons/XSquareRoundedIcon'
+
 
 const Notification = () => {
   const { show, message, type } = useNotificationContext()
-
-  if (!show) return null
 
   const typeStyles = {
     error: 'bg-gray-50 text-red-600 border border-red-500',
@@ -14,15 +16,29 @@ const Notification = () => {
   }
 
   return createPortal(
-    <ComponentTransition>
-    <div
-      className={`fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-        px-6 py-3 rounded shadow-lg text-center font-semibold 
-        ${typeStyles[type] || 'bg-gray-100 text-gray-700 border border-gray-400'}`}
-    >
-      {message}
-    </div>
-    </ComponentTransition>,
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          key="notification"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.4, ease: 'easeInOut' }}
+          className={`fixed z-50 bottom-4 right-4 flex flex-row place-items-center
+            px-3 py-3 rounded shadow-lg font-semibold text-base text-right
+            max-w-xs
+            ${typeStyles[type] || 'bg-gray-100 text-gray-700 border border-gray-400'}`}
+        >
+          {type === 'error' ? (
+            <XSquareRoundedIcon className='mr-2' />
+          ) : type === 'success' ? (
+            <CheckRoundedIcon className='mr-2' />
+          ) : type === 'info' ? (
+            <AlertRoundedIcon className='mr-2' />
+          ) : null} {message}
+        </motion.div>
+      )}
+    </AnimatePresence>,
     document.body
   )
 }
