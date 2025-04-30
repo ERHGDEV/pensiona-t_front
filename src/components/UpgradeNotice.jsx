4f72c1083808f82e6c1eae584080e6c81f74dfe3
ERHGDEV <erhgdev@gmail.com>
2025-04-30 15:59:17 -0600

    Se ajusta tamaño de icon

--- src/components/UpgradeNotice.jsx
+++ src/components/UpgradeNotice.jsx
@@ -3,7 +3,7 @@ import InfoSquareRoundedIcon from "./icons/InfoSquareRoundedIcon"
 const UpgradeNotice = ({ onSelection }) => {
   return (
     <div className="bg-white border-l-4 border-sky-600 p-4 mt-4 rounded-xl shadow-md flex items-start gap-2">
-      <InfoSquareRoundedIcon className="text-sky-600 w-14 my-auto" />
+      <InfoSquareRoundedIcon className="text-sky-600 w-16 my-auto" />
       <p className="text-sky-900 text-base leading-relaxed">
         Con el plan <span className="font-semibold text-yellow-700">Free</span> solo puedes hacer{" "}
         <span className="font-semibold text-yellow-700">una consulta de Afore al día</span>.{" "}
