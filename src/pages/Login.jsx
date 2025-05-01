7ef5badbe42a81c1aece8b29cee15b07984ac1a2
ERHGDEV <erhgdev@gmail.com>
2025-05-01 09:49:51 -0600

    Se setea activesection si la sesión ya está iniciada

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,9 @@ import { useNavigate } from "react-router-dom"
 import authService from "../services/authService"
 import Dots from "../components/Dots"
 import Button from "../components/Button"
-import Footer from "../components/Footer"
 import { Link } from "react-router-dom"
 import { useNotificationContext } from "../context/NotificationContext"
+import { useActiveSection } from "../context/ActiveSectionContext"
 
 const Login = () => {
   const [email, setEmail] = useState('')
@@ -13,6 +13,7 @@ const Login = () => {
   const [loading, setLoading] = useState(false)
   const [statusMessage, setStatusMessage] = useState('')
 
+  const { setActiveSection } = useActiveSection()
   const { showNotification } = useNotificationContext()
   const navigate = useNavigate()
   const userIsAuthenticated = authService.isAuthenticated()
@@ -21,7 +22,10 @@ const Login = () => {
     if (userIsAuthenticated) {
       const role = authService.getUserRole()
       if (role === 'admin') navigate('/admin')
-      else navigate('/user')
+      else {
+        setActiveSection('calculator')
+        navigate('/user')
+      }
     }
   }, [])
 
@@ -39,6 +43,7 @@ const Login = () => {
             if (response.role === 'admin') {
                 navigate('/admin')
             } else {
+                setActiveSection('calculator')
                 navigate('/user')
             }
         } else {
@@ -60,103 +65,99 @@ const Login = () => {
   }
 
   return (
-    <>
-      <main>
-        <form
-          onSubmit={handleSubmit} 
-          autoComplete='off' 
-          className="max-w-sm mx-auto flex flex-col justify-center h-[calc(100vh-80px)]"
-        >
-          <p className='text-xl mb-5'>Inicia sesión</p>
-          <div className="relative z-0 w-full mb-5 group">
-            <input 
-              type="email" 
-              name="email" 
-              id="email" 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="
-                  block py-2.5 px-0 w-full 
-                  text-md bg-transparent 
-                  border-0 border-b-2 border-gray-500 
-                  appearance-none 
-                  focus:outline-none focus:ring-0 
-                  focus:border-gray-100 peer" 
-              placeholder=" " 
-              required 
-            />
-            <label 
-              htmlFor='email'
-              className="
-                absolute text-md 
-                text-gray-400 duration-300 
-                transform -translate-y-6 scale-90 top-3 -z-10 
-                origin-[0] peer-focus:start-0 
-                rtl:peer-focus:translate-x-1/2 
-                rtl:peer-focus:left-auto peer-focus:text-gray-100 
-                peer-placeholder-shown:scale-100 
-                peer-placeholder-shown:translate-y-0 
-                peer-focus:scale-90 peer-focus:-translate-y-6"
-            >
-              Correo electrónico
-            </label>
-          </div>
-          <div className="relative z-0 w-full mb-5 group">
-            <input 
-              type="password" 
-              name="password" 
-              id="password" 
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="
+    <main>
+      <form
+        onSubmit={handleSubmit} 
+        autoComplete='off' 
+        className="max-w-sm mx-auto flex flex-col justify-center h-[calc(100vh-80px)]"
+      >
+        <p className='text-xl mb-5'>Inicia sesión</p>
+        <div className="relative z-0 w-full mb-5 group">
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="
                 block py-2.5 px-0 w-full 
                 text-md bg-transparent 
                 border-0 border-b-2 border-gray-500 
                 appearance-none 
                 focus:outline-none focus:ring-0 
                 focus:border-gray-100 peer" 
-              placeholder=" " 
-              required 
-            />
-            <label 
-              htmlFor='password'
-              className="
-                absolute text-md 
-                text-gray-400 duration-300 
-                transform -translate-y-6 scale-90 top-3 -z-10 
-                origin-[0] peer-focus:start-0 
-                rtl:peer-focus:translate-x-1/2 
-                rtl:peer-focus:left-auto peer-focus:text-gray-100 
-                peer-placeholder-shown:scale-100 
-                peer-placeholder-shown:translate-y-0 
-                peer-focus:scale-90 peer-focus:-translate-y-6"
-            >
-              Password
-            </label>
-          </div>
-          <Link 
-            className="text-sm text-gray-300 hover:text-white active:text-gray-500
-              transition duration-300 ease-in-out cursor-pointer" 
-            to="/password">
-              Olvidé mi contraseña
-          </Link>
-          <div className="mt-8 flex flex-col sm:flex-row gap-4 max-w-fit mx-auto">
-            <Button type="submit" order="primary" disabled={loading}>
-              {loading ? (
-                <span className="text-center">
-                  {statusMessage}
-                  <Dots color='true' />
-                </span>
-              ) : (
-                'Entrar'
-              )}
-            </Button>
-          </div>
-        </form>
-      </main>
-
-      {/* <Footer variant="fixed"/> */}
-    </>
+            placeholder=" " 
+            required 
+          />
+          <label 
+            htmlFor='email'
+            className="
+              absolute text-md 
+              text-gray-400 duration-300 
+              transform -translate-y-6 scale-90 top-3 -z-10 
+              origin-[0] peer-focus:start-0 
+              rtl:peer-focus:translate-x-1/2 
+              rtl:peer-focus:left-auto peer-focus:text-gray-100 
+              peer-placeholder-shown:scale-100 
+              peer-placeholder-shown:translate-y-0 
+              peer-focus:scale-90 peer-focus:-translate-y-6"
+          >
+            Correo electrónico
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="
+              block py-2.5 px-0 w-full 
+              text-md bg-transparent 
+              border-0 border-b-2 border-gray-500 
+              appearance-none 
+              focus:outline-none focus:ring-0 
+              focus:border-gray-100 peer" 
+            placeholder=" " 
+            required 
+          />
+          <label 
+            htmlFor='password'
+            className="
+              absolute text-md 
+              text-gray-400 duration-300 
+              transform -translate-y-6 scale-90 top-3 -z-10 
+              origin-[0] peer-focus:start-0 
+              rtl:peer-focus:translate-x-1/2 
+              rtl:peer-focus:left-auto peer-focus:text-gray-100 
+              peer-placeholder-shown:scale-100 
+              peer-placeholder-shown:translate-y-0 
+              peer-focus:scale-90 peer-focus:-translate-y-6"
+          >
+            Password
+          </label>
+        </div>
+        <Link 
+          className="text-sm text-gray-300 hover:text-white active:text-gray-500
+            transition duration-300 ease-in-out cursor-pointer" 
+          to="/password">
+            Olvidé mi contraseña
+        </Link>
+        <div className="mt-8 flex flex-col sm:flex-row gap-4 max-w-fit mx-auto">
+          <Button type="submit" order="primary" disabled={loading}>
+            {loading ? (
+              <span className="text-center">
+                {statusMessage}
+                <Dots color='true' />
+              </span>
+            ) : (
+              'Entrar'
+            )}
+          </Button>
+        </div>
+      </form>
+    </main>
   )
 }
 
